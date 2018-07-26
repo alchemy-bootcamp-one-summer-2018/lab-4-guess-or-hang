cce0514134fc0395f8ff74c6e7dc3c087023cd59
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-07-26 13:43:56 -0700

    got random index

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 /*global words*/
 /* exported guess*/
+var guesses = 0;
 
-function guess() {
+function loadWord() {
+    var pullWord = words.slice();
 
+    guesses += 1;
+
+    function getRandomWord (){
+        var index = getRandomIndex(pullWord.length);
+        var words = pullWord[index];
+        pullWord.splice(index, 1);
+
+        return words;
+    }
 }
 
+function guess() {
+    
+}
+
+function getRandomIndex(max) {
+    return Math.floor(Math.random() * max);
+}
+
+console.log('random index', getRandomIndex(5));
 console.log('app.js is called');
--- words.js
+++ words.js
@@ -1,6 +1,10 @@
 /*exported words*/
 
 var words = [
-    ''
+    'error',
+    'problem',
+    'incorrect',
+    'fatal',
+    'horrible',
 ];
 console.log('words.js is called');
