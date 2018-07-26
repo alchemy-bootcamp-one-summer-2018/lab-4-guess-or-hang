0fb21150a960009a9fabc7fcdb5e64c6b433165c
laurenshareshian <laurenshareshian@gmail.com>
2018-07-26 14:41:05 -0700

    guess fcn through step 4

--- app-together.js
+++ app-together.js
@@ -1,11 +1,10 @@
-
+var letterList = [];
 
 function loadWord(words) {
     console.log('what words?', words);
     var randNum = getRanInteger(0, words.length);
 
     var word = words[randNum];
-    console.log('random word:', word);
 
     var wordResult = document.getElementById('word-results');
     wordResult.innerText = word;
@@ -16,5 +15,54 @@ function loadWord(words) {
 
     }
 
-    return false;
+    return word;
+}
+
+function guess(words){
+    //picked a word
+    var word = loadWord(words); 
+    console.log('random word:', word);
+
+    // takes in guess letter and stores it
+    var form = document.getElementById('hangman'); 
+    var elements = form.elements;
+    var guess = elements.guessletter.value;
+    console.log('guess:', guess);
+    
+    // make sure guess is a letter
+    checkLetter(guess);
+
+    // check guess is a repeat (true or false)
+    var repeat = checkRepeat(guess);
+    console.log('repeat:', repeat);
+
+    // if not a repeat, add to letterList
+    if(repeat === false){
+        letterList.push(guess);
+        console.log('letterList:', letterList)
+    }
+
+    return false; // doesnt clear
+}
+
+function checkLetter(character)
+{
+    var regex = /^[a-zA-Z]+$/;
+    if(!character.match(regex))
+    {
+        console.log("Must input letters"); 
+        // alert("Must input letters");
+
+    }
+}
+
+function checkRepeat(guess){
+    if(letterList.includes(guess)){
+        console.log("You already guessed this letter!"); 
+        // alert("You already guessed this letter!");
+        return true;
+    }
+    else {
+        return false;
+    }
 }
--- js/words.js
+++
@@ -1,1 +0,0 @@
-var words = ['hello', 'soccer', 'dog'];
