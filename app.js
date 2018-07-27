447ceef97455349c672b338b3aff8f4b412c71e2
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-07-27 16:25:35 -0700

    handle correct guesses

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var numberOfGuesses = 0;
 var getArray = words.slice();
 var selectedWord = '';
 var lettersGuessed = [];
-var correctGuesses = [];
+var correctLetters = [];
+var guessedLetter = document.querySelector('input').value;
 console.log('app.js is working');
 
 function loadWord() {
@@ -24,28 +25,51 @@ function showBlanks() {
 }
 loadWord();
 showBlanks();
+document.getElementById('number-of-guesses').innerText = numberOfGuesses;
+//document.getElementById('body-').hidden = false;
+
 function guess() {
     
-    var guessedLetter = document.querySelector('input').value;
     console.log('letter pressed:', guessedLetter);
-
-    if(lettersGuessed.includes(guessedLetter)) {
-        alert('ERROR: Travis noticed you have already used ' + guessedLetter);
-    }
-    else {
-        lettersGuessed.push(guessedLetter);
-        numberOfGuesses ++; 
-        document.getElementById('number-of-guesses').innerText = numberOfGuesses;
-        document.getElementById('body-').hidden = false;
+    
+    if(selectedWord.includes(guessedLetter)) {
+        
+        //show in correct blank space 
+        //find matching index for correct letter guessed
+        letterLocation();
+        
+        //put into array of correct guesses
+        
+        // count as a guess
+        
+        //show the letter in guessed letters
+        document.getElementById('guessed-letter').innerText = lettersGuessed;
+        
+        //check for win
+        //alert for win
     }
     
-    console.log('letter guessed:', lettersGuessed);
-    document.getElementById('guessed-letter').innerText = lettersGuessed;
+    else {
+        //count as guess
+        //show the letter in guessed letters
+        //put into array of incorrect guesses (6)
+        //reveal a body part
+        //check for loss
+        //alert loss
+    }
+    
+}
 
+function handleCorrectGuesses() {
+    correctLetters.push(guessedLetter);
+    numberOfGuesses ++; 
+}
+
+function letterLocation() {
     for(var i = 0; i < selectedWord.length; i++) {
-
+                
         console.log('test-letter', selectedWord[0]);
-
+                
         if(selectedWord[i] === guessedLetter) {
             document.getElementById('letter-' + i).innerText = guessedLetter;
         }        
@@ -53,8 +77,16 @@ function guess() {
     return false;
 }
 
+function isPreviouslyGuessed() {
+    if(lettersGuessed.includes(guessedLetter)) {
+        alert('ERROR: Travis noticed you have already used ' + guessedLetter);
+    }
+   
+    else {
+        lettersGuessed.push(guessedLetter);
+    }
+}
+
 function getRandomIndex(max) {
     return Math.floor(Math.random() * max);
 }
-
-console.log('random index', getRandomIndex(5));
