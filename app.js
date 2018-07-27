1151f028e4a86c381fc1813d7a1faf74dbd89ae9
Mike <mbennettillustration@gmail.com>
2018-07-26 17:52:12 -0700

    array of letters appear

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var guesses = 0;
 var getArray = words.slice();
 var selectedWord = '';
 var wordToGuess = [];
+var lettersGuessed = [];
+//var span = document.getElementById('guessed-letter-one');
 
 function loadWord() {
     var index = getRandomIndex(getArray.length);
     selectedWord = getArray[index];
     getArray.splice(index, 1);
     console.log('word', selectedWord);
-        
+    
     return selectedWord;
 }
 function showBlanks() {
@@ -25,10 +27,10 @@ function showBlanks() {
 loadWord();
 showBlanks();
 function guess() {
-    //document.querySelector.form.elements;
-    //console.log('letter pressed:', document.querySelector('form').elements);
-    console.log('letter pressed:', document.querySelector('input').value);
-
+    var guessedLetter = document.querySelector('input').value;
+    console.log('letter pressed:', guessedLetter);
+    lettersGuessed.push(guessedLetter);
+    console.log('letter guessed:', lettersGuessed);
     return false;
 }
 
