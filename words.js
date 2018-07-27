00946db2b2eb7a93f86ab13f7d79ae229e53f954
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-07-26 19:53:07 -0700

    comments

--- words.js
+++ words.js
@@ -1,10 +1,10 @@
 /*exported words*/
 
 var words = [
-    'error',
-    'problem',
-    'incorrect',
-    'fatal',
-    'horrible',
+    'error', //0
+    'problem', //1
+    'incorrect', //2
+    'fatal', //3
+    'horrible', //4
 ];
 console.log('words.js is called');
