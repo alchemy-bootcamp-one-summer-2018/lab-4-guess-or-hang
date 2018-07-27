e6ca4c912b04e63304ead6f081d9ddbd4caa89d3
kkrosefields <kkrosefields@gmail.com>
2018-07-27 15:03:23 -0700

    added function

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
 var index = null;
 var selectedWord = '';
 
+
+
 //Choosing a random word. line 13 is creating a variable that is taking the function getRandomIndex and are 
 // array of words line 17 we created a  
 function getRandomWord() {
@@ -41,12 +43,44 @@ function getRandomIndex(max) {
     return Math.floor(Math.random() * max);
 }
 
-//This shows the the random word
+//This is the guess form function
 
+var guessForm = document.getElementById ('guess-form');
+var guessResult = document.getElementById ('guess-result');
+// var totalGuesses = 0;
+
+function guess(){
+
+    var letterTheyGuessed = guessForm.elements.guessLetter.value;
+    console.log (letterTheyGuessed);
+
+    if(letterTheyGuessed === 'cat') {
+        guessResult.innerText = 'That\'s right! An ' + letterTheyGuessed;
+        console.log ('this is what user guessed', letterTheyGuessed);
+    }
+
+    return false;
+
+}
+
+
+
+
+
+
+
+//     else {
+
+//         totalGuesses = totalGuesses + 1;
+//         console.log (totalGuesses);
+//         guessResult.innerText = 'Whomp Whomp... a ' + letterTheyGuessed +
+//         ' is not my favorite. You\'re closer though. You\'ve guessed ' +
+//         totalGuesses + ' times try again!';
+//     }
+// }
 
 
 
 
 //Loop: an empty array called answerArray and fill it with
 //underscores (_) to match the number of letters in the word.
-for (var j)
