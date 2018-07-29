248b1f93f1f3174fe13f87c7415096e3748620f7
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-07-29 08:26:10 -0700

    handle correct guesses

--- app.js
+++ app.js
@@ -9,12 +9,24 @@ var lettersGuessed = [];
 var correctLetters = [];
 var guessedLetter = ''; 
 var incorrectGuesses = 0;
+var targetLettersArr = [];
 
+function getUniqueChars(word) {
+    var wordArr = word.split('');
+    var uniqueLetters = [];
+    wordArr.forEach(char => {
+        if(uniqueLetters.indexOf(char) === -1) {
+            uniqueLetters.push(char);
+        }
+    });
+    return uniqueLetters.sort();
+}
 
 function loadWord() {
     var index = getRandomIndex(getArray.length);
     selectedWord = getArray[index];
     getArray.splice(index, 1);
+    targetLettersArr = getUniqueChars(selectedWord);
     console.log('word', selectedWord);
     
     return selectedWord;
@@ -23,54 +35,81 @@ function showBlanks() {
     console.log('selected word letters:', selectedWord.length);
     for(var i = 0; i < selectedWord.length; i++) {
         document.getElementById('letter-' + i).innerText = '_';
+
         //correctLetters.join(checkCorrect);
         //console.log('spelling-word', correctLetters.join(checkCorrect));
     }
 }
-loadWord();
-showBlanks();
+
+function startNewGame() {
+    numberOfGuesses = 0;
+    getArray = words.slice();
+    selectedWord = '';
+    checkCorrect = [];
+    lettersGuessed = [];
+    correctLetters = [];
+    guessedLetter = ''; 
+    incorrectGuesses = 0;
+    targetLettersArr = [];
+    loadWord();
+    showBlanks();
+
+}
 
 function guess() {
     guessedLetter = document.querySelector('input').value;
     console.log('GUESSED LETTER: ', guessedLetter);
     
-    if(selectedWord.includes(guessedLetter)) {
+    if(!lettersGuessed.includes(guessedLetter)) {
+
+        if(selectedWord.includes(guessedLetter)) {
+            
+            lettersGuessed.push(guessedLetter);
+            document.getElementById('guessed-letter').innerText = lettersGuessed;
+            console.log('letter pressed:', guessedLetter);
+            
+            console.log('reached correct guess');
+            //show in correct blank space 
+            //find matching index for correct letter guessed
+            letterLocation();
+            
+            // count as a guess
+            handleCorrectGuesses();
+            console.log('correct letters', correctLetters.sort());
+            console.log('target letters', targetLettersArr);
+            console.log('number of guesses', numberOfGuesses);
+            //show the letter in guessed letters
+            document.getElementById('number-of-guesses').innerText = numberOfGuesses;
+            
+            //check for win
+            
+            if(correctLetters.sort().join() === targetLettersArr.join()) {
+                console.log('you won');
+                alert('You Won!!');
+                document.getElementById('submit-button').disabled = true;
+                document.getElementById('play-again').hidden = false;
+                
+            }
+            //correctLetters.join('');
+            //console.log('spelling word: ', correctLetters.join(''));
+            //alert for win
+            
+        }
         
-        lettersGuessed.push(guessedLetter);
-        document.getElementById('guessed-letter').innerText = lettersGuessed;
-        console.log('letter pressed:', guessedLetter);
-        
-        console.log('reached correct guess');
-        //show in correct blank space 
-        //find matching index for correct letter guessed
-        letterLocation();
-        
-        // count as a guess
-        handleCorrectGuesses();
-        console.log('correct letters', correctLetters);
-        console.log('number of guesses', numberOfGuesses);
-        //show the letter in guessed letters
-        document.getElementById('number-of-guesses').innerText = numberOfGuesses;
-        
-        //check for win
-        correctLetters.join('');
-        console.log('spelling word: ', correctLetters.join(''));
-        //alert for win
-    }
-    
-    else {
-        //count as guess
-        document.getElementById('number-of-guesses').innerText = numberOfGuesses;
-        //show the letter in guessed letters
-        lettersGuessed.push(guessedLetter); 
-        document.getElementById('guessed-letter').innerText = lettersGuessed;
-        //put into array of incorrect guesses (6)
-        incorrectGuesses++;
-        console.log('incorrect guesses:', incorrectGuesses);
-        //reveal a body part
-        document.getElementById('body-' + incorrectGuesses).hidden = false;
-        //check for loss
-        //alert loss
+        else {
+            //count as guess
+            document.getElementById('number-of-guesses').innerText = numberOfGuesses;
+            //show the letter in guessed letters
+            lettersGuessed.push(guessedLetter); 
+            document.getElementById('guessed-letter').innerText = lettersGuessed;
+            //put into array of incorrect guesses (6)
+            incorrectGuesses++;
+            console.log('incorrect guesses:', incorrectGuesses);
+            //reveal a body part
+            document.getElementById('body-' + incorrectGuesses).hidden = false;
+            //check for loss
+            //alert loss
+        }
     }
     return false;
 }
@@ -86,11 +125,17 @@ function letterLocation() {
         console.log('test-letter', selectedWord[0]);
                 
         if(selectedWord[i] === guessedLetter) {
-            lettersGuessed.push(guessedLetter);
             document.getElementById('letter-' + i).innerText = guessedLetter;
+
         }        
     }
     return false;
+}
+
+startNewGame();
+
+function checkWinCondition() {
+    
 }
 
 function isPreviouslyGuessed() {
